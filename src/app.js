3851104d64cddf5bba7aa9b3f51c1657d74a4283
MarioSeattle <marioseattle@live.com>
2017-09-04 17:29:10 -0700

    Importing sidebar without giving any properties and no need for actions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers } from 'redux'
-import { addDeck, showAddDeck, hideAddDeck } from './actions';
 import { Provider } from 'react-redux';
 import * as reducers from './reducers';
 import App from './components/app';
@@ -15,18 +14,11 @@ const store = createStore(combineReducers(reducers));
 function run () {
     let state = store.getState();
     console.log(state);
-    ReactDOM.render((
-            <Provider store={store}>
-            <App>
-        <Sidebar
-            decks={ state.decks }
-            addingDeck={ state.addingDeck }
-            addDeck={name => store.dispatch(addDeck(name))}
-            showAddDeck={() => store.dispatch(showAddDeck())}
-            hideAddDeck={() => store.dispatch(hideAddDeck())}
-        />
-            </App>
-            </Provider>), document.getElementById('root'));
+    ReactDOM.render((<Provider store={store}>
+        <App>
+            <Sidebar />
+        </App>
+    </Provider>), document.getElementById('root'));
 
 }
 //run the function
